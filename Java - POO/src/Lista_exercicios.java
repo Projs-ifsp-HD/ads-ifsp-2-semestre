3b7e0167677f2a370c9b414b46c955d38fcaaa63
Hamilton Junior <hamiltonspjunior@gmail.com>
2018-02-11 14:39:49 -0200

    Feito até Ex6

--- Java - POO/src/Lista_exercicios.java
+++ Java - POO/src/Lista_exercicios.java
@@ -2,7 +2,7 @@
 import java.util.Scanner;
 public class Lista_exercicios {
 	public static void main(String[] args) {
-		Ex3();
+		Ex6();
 	}
 	
 	private static int Ex1(){
@@ -31,6 +31,62 @@ public class Lista_exercicios {
 		if(n > 50){
 			System.out.print("O número é maior que 50");
 		}
+		return 0;
+	}
+	
+	private static float Ex4(){
+		float n;
+		Scanner ler = new Scanner(System.in);
+		System.out.print("Digite um número: ");
+		n = ler.nextFloat();
+		if(n > 100){
+			System.out.print("O número é maior que 100");
+		}else{
+			System.out.print("O valor é menor ou igual a 100");
+		}
+		return 0;
+	}
+	
+	private static float Ex5(){
+		float n;
+		Scanner ler = new Scanner(System.in);
+		System.out.print("Digite um número: ");
+		n = ler.nextFloat();
+		if(n %2 == 0){
+			System.out.print("O número é par!");
+		}else{
+			System.out.print("O número é impar!");
+		}
+		return 0;
+	}
+
+	private static float Ex6(){
+		float a, b;
+		char resA = 'V', resB = 'V', resAB = 'F';
+		Scanner ler = new Scanner(System.in);
+		System.out.print("Digite um número para A: ");
+		a = ler.nextFloat();
+		
+		System.out.print("Digite um número para B: ");
+		b = ler.nextFloat();
+		
+		if(a < 0){
+			resA = 'F';
+		}
+		
+		if(b < 0){
+			resB = 'F';
+		}
+		
+		if(resA == 'F'){
+			resAB = 'F';
+		}
+		
+		System.out.print("\n" +
+						"("+ resA +") A é maior que zero? \n" +
+						"("+ resB +") B é maior que zero? \n" +
+						"("+ resAB +") A e B é maior que zero?");
+		
 		return 0;
 	}
 }
