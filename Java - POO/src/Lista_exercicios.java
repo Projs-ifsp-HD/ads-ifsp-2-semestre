fe9c8636fb91e58f7013d12370eedf5820c855f0
Hálex Dyllan <halexdx@gmail.com>
2018-02-11 18:30:15 -0200

    questõe até a 12

--- Java - POO/src/Lista_exercicios.java
+++ Java - POO/src/Lista_exercicios.java
@@ -2,8 +2,15 @@
 import java.util.Scanner;
 public class Lista_exercicios {
 	public static void main(String[] args) {
-		Ex6();
+	//digite o execicio que deseja .
+		
+		Ex11();
+		
+		
 	}
+	
+	
+	
 	
 	private static int Ex1(){
 		int n;
@@ -20,17 +27,6 @@ public class Lista_exercicios {
 		System.out.print("Digite um número: ");
 		n = ler.nextFloat();
 		System.out.print("Número digitado: " + n);
-		return 0;
-	}
-	
-	private static float Ex3(){
-		float n;
-		Scanner ler = new Scanner(System.in);
-		System.out.print("Digite um número: ");
-		n = ler.nextFloat();
-		if(n > 50){
-			System.out.print("O número é maior que 50");
-		}
 		return 0;
 	}
 	
@@ -64,34 +60,179 @@ public class Lista_exercicios {
 		float a, b;
 		char resA = 'V', resB = 'V', resAB = 'F';
 		Scanner ler = new Scanner(System.in);
-		System.out.print("Digite um número para A: ");
+		System.out.print("Digite um numero para A: ");
 		a = ler.nextFloat();
 		
-		System.out.print("Digite um número para B: ");
+		System.out.print("Digite um numero para B: ");
 		b = ler.nextFloat();
-		
-		if(a > 0 && b > 0){
-			resAB = 'V';
-		}else{
-			resAB = 'F';
-		}
 		
 		if(a <= 0){
 			resA = 'F';
-		}else{
-			resA = 'V';
 		}
 		
 		if(b <= 0){
 			resB = 'F';
-		}else{
-			resB = 'V';
+		}
+		
+		if(resA == 'V' && resB == 'V'){
+			resAB = 'V';
 		}
 		
 		System.out.print("\n" +
-						"("+ resA +") A é maior que zero? \n" +
-						"("+ resB +") B é maior que zero? \n" +
-						"("+ resAB +") A e B é maior que zero?");
+						"("+ resA +") A e maior que zero? \n" +
+						"("+ resB +") B e maior que zero? \n" +
+						"("+ resAB +") A e B e maior que zero?");
+		
+		return 0;
+	}
+	
+	private static float Ex7(){
+		float a, b;
+		char resA = 'V', resB = 'V', resAB = 'F';
+		Scanner ler = new Scanner(System.in);
+		System.out.print("Digite um numero para A: ");
+		a = ler.nextFloat();
+		
+		System.out.print("Digite um numero para B: ");
+		b = ler.nextFloat();
+		
+		if(a != 0){
+			resA = 'F';
+		}
+		
+		if(b >= 0){
+			resB = 'F';
+		}
+		
+		if(a > 0 || b > 0){
+			resAB = 'V';
+		}
+		
+		System.out.print("\n" +
+						"("+ resA +") A e igual zero? \n" +
+						"("+ resB +") B e menor que zero? \n" +
+						"("+ resAB +") A ou B sao maiores que zero?");
+		
+		return 0;
+	}
+	
+	private static int Ex8(){
+		int n,n2,n3,maior;
+		Scanner ler = new Scanner(System.in);
+		
+		System.out.print("Digite o primeiro numero: ");
+		n = ler.nextInt();
+		
+		System.out.print("Digite o segundo numero: ");
+		n2 = ler.nextInt();
+		
+		System.out.print("Digite o terceiro numero: ");
+		n3 = ler.nextInt();
+		
+		if(n > n2){
+		    maior = n;
+		}
+		else{
+		    maior = n2;
+		}
+
+		if(maior < n3){
+		    maior = n3;
+		}
+		
+		System.out.print("O maior valor digitado e: " + maior);
+
+		
+		return 0;
+	}
+	
+	private static float Ex9(){
+		float n,n2,n3,maior;
+		Scanner ler = new Scanner(System.in);
+		
+		System.out.print("Digite o primeiro numero: ");
+		n = ler.nextFloat();
+		
+		System.out.print("Digite o segundo numero: ");
+		n2 = ler.nextFloat();
+		
+		System.out.print("Digite o terceiro numero: ");
+		n3 = ler.nextFloat();
+		
+		if(n > n2){
+		    maior = n;
+		}
+		else{
+		    maior = n2;
+		}
+
+		if(maior < n3){
+		    maior = n3;
+		}
+		
+		System.out.print("O maior valor digitado e: " + maior);
+
+		
+		return 0;
+	}
+	
+	private static float Ex10(){
+		float n,n2,n3;
+		Scanner ler = new Scanner(System.in);
+		
+		System.out.print("Digite o primeiro numero: ");
+		n = ler.nextFloat();
+		
+		System.out.print("Digite o segundo numero: ");
+		n2 = ler.nextFloat();
+		
+		System.out.print("Digite o terceiro numero: ");
+		n3 = ler.nextFloat();
+		
+		if(n > n2) {
+			if(n2 > n3) {
+				System.out.print("Ordem Crescente:  "+ n +" - "+ n2 +" - "+ n3 +"");
+			}else if(n > n3) {
+				System.out.print("Ordem Crescente:  "+ n +" - "+ n3 +" - "+ n2 +"");
+			}else {
+				System.out.print("Ordem Crescente:  "+ n3 +" - "+ n +" - "+ n2 +"");
+			}
+		}else if(n2 > n3) {
+			if(n > n3) {
+				System.out.print("Ordem Crescente: "+ n2 +" - "+ n +" - "+ n3 +"");
+			}else {
+				System.out.print("\"Ordem Crescente: "+ n2 +" - "+ n3 +" - "+ n +"");
+			}
+		}else {
+			System.out.print("Ordem Crescente: "+ n3 +" - "+ n2 +" - "+ n +"");
+		}
+		
+		
+		return 0;
+	}
+	
+	private static int Ex11(){
+		//Escreva um programa que leia dois n�meros inteiros A e B quaisquer indicando se A � m�ltiplo de B ou se B � m�ltiplo de A.
+		float a,b;
+		
+		Scanner ler = new Scanner(System.in);
+		
+		System.out.print("Digite o valor de A: ");
+		a = ler.nextInt();
+		
+		System.out.print("Digite o valor de B: ");
+		b = ler.nextInt();
+		
+		if( (a % b) == 0) {
+			System.out.print("A e multiplo de B");
+		}
+		else if( (b % a) == 0) {
+			System.out.print("B e multiplo de A ");
+		}
+		else {
+			System.out.print("B nao e multiplo de  A | e A nao e multiplo de B ");
+		}
+		
 		
 		return 0;
 	}
