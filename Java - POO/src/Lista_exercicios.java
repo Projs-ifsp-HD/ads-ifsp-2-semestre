75032027dab2bec65da6964c025e8c2d39ba0b89
Hálex Dyllan <halexdx@gmail.com>
2018-02-12 00:39:36 -0200

    12 + 14 +16

--- Java - POO/src/Lista_exercicios.java
+++ Java - POO/src/Lista_exercicios.java
@@ -4,7 +4,7 @@ public class Lista_exercicios {
 	public static void main(String[] args) {
 	//digite o execicio que deseja .
 		
-		Ex6();
+		Ex16();
 		
 		
 	}
@@ -219,7 +219,7 @@ public class Lista_exercicios {
 		return 0;
 	}
 	
-	private static int Ex11(){
+	private static float Ex11(){
 		//Escreva um programa que leia dois nï¿½meros inteiros A e B quaisquer indicando se A ï¿½ mï¿½ltiplo de B ou se B ï¿½ mï¿½ltiplo de A.
 		float a,b;
 		
@@ -245,10 +245,75 @@ public class Lista_exercicios {
 		return 0;
 	}
 	
-	private static int Ex12(){
-		//Escreva um programa que leia dois nï¿½meros inteiros A e B quaisquer indicando se A ï¿½ mï¿½ltiplo de B ou se B ï¿½ mï¿½ltiplo de A.
-		System.out.print("Teste");
-		System.out.print("Teste2");
+	private static float Ex12(){
+		/*Escreva um programa capaz de calcular o preço total a ser pago por uma compra de copos plásticos. O usuário deve
+		fornecer o número de copos a serem comprados e o programa deve calcular o preço total a ser cobrado, exibindo-o.
+		Observe que: se o número de copo é inferior ou igual a 100, o preço por copo é R$0.05; se o número de copos está entre
+		101 e 500, o preço por copo é R$0.04; finalmente se o número de copos é superior a 500 o preço por copo é R$0.035.*/ 
+		float total, copos;
+		Scanner ler = new Scanner(System.in);
+		
+		System.out.print("Digite o valor a Quantidade de Copos que deseja:  ");
+		copos = ler.nextFloat();
+		
+		if(copos <= 100) {
+			total = copos * 0.05f;
+		}
+		else if((copos > 100) && (copos <= 500)) {
+			total = copos * 0.04f;
+		}else {
+			total = copos * 0.035f;
+		}
+		
+		System.out.print("O valor a ser pago em "+ copos +" é : " + total);
+		
+		return 0;
+	}
+	
+	private static float Ex14(){
+		/*Escreva um programa que leia 3 notas (valores reais), calculando e exibindo sua média aritmética. Imprima também
+		"Aprovado" se a média for maior que 7, "Reprovado" se for menor que 3 e "Exame" se estiver entre 3 e 7.*/ 
+		float nota,nota2,nota3,media;
+		Scanner ler = new Scanner(System.in);
+		
+		System.out.print("Digite a primeira Nota: ");
+		nota = ler.nextFloat();
+		
+		System.out.print("Digite a segunda Nota: ");
+		nota2 = ler.nextFloat();
+		
+		System.out.print("Digite a terceiro Nota: ");
+		nota3 = ler.nextFloat();
+		
+		media = (nota + nota2 +nota3)/3;
+		
+		if(media >= 7) {
+			System.out.print("O aluno foi Aprovado com a nota de : " + media);
+		}else if(media <= 3) {
+			System.out.print("O aluno foi Reprovado com a nota de  : " + media);
+		}else {
+			System.out.print("O aluno esta de Exame com a nota de  : " + media);
+		}
+		
+		return 0;
+	}
+	
+	private static float Ex16(){
+		/*. Preparar um programa para ler as medidas da base e da altura de um triângulo, calculando e imprimindo sua área,
+			sabendo que o cálculo da área é dado por: area = (base*altura)/2*/ 
+		float area,altura,base;
+		Scanner ler = new Scanner(System.in);
+		
+		System.out.print("Digite a altura do triangulo: ");
+		altura = ler.nextFloat();
+		
+		System.out.print("Digite a base do triangulo: ");
+		base = ler.nextFloat();
+		
+		area = (base * altura)/2;
+		
+			System.out.print("A area do seu triangulo e  : " + area);
+		
 		return 0;
 	}
 }
