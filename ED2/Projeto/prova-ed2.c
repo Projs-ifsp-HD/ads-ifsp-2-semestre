129cc7e02597d0b4b5b1519e27d63f181d2faab6
Hamiltonspjunior <hamiltonspjunior@gmail.com>
2018-04-14 16:23:41 -0300

    Merge do menu e dos algoritimos de ordenação

--- ED2/Projeto/prova-ed2.c
+++ ED2/Projeto/prova-ed2.c
@@ -6,7 +6,6 @@
 #define MAX 10
 #define N 7 //Declaração da constante
 
-
 //Protótipos
 void bubleSort(int *v,int n);
 void selectionSort(int *v,int n);
@@ -22,35 +21,153 @@ void exibeVet(int *vet, int n);
 
 int main(){
     setlocale(LC_ALL,"");
-    int v[10]={180,190,170,110,100,140,130,120,160,150};
-    int n = 10;
-    setlocale(LC_ALL,"");
-	printf("\nVetor Ordenado:\n");
-    ///bubleSort(v , n);
-    ///selectionSort(v , n);
-    ///insertionSort(v, n);
-    ///shellSort(v, n);
-    /**mergeSort(0, 10, v);
-    exibeVet(v, n);
-    }*/
+    /*
+        int v[10]={180,190,170,110,100,140,130,120,160,150};
+        int n = 10;
+        printf("\nVetor Ordenado:\n");
+        bubleSort(v , n);
+        selectionSort(v , n);
+        insertionSort(v, n);
+        shellSort(v, n);
+        mergeSort(0, 10, v);
+        exibeVet(v, n);
+        }
 
-    /**for(int i = 0; i < n; i++){
-      ordena_quicksort(v, 0, n - 1);
-      printf("%d ", v[i]);
-    }*/
+        for(int i = 0; i < n; i++){
+          ordena_quicksort(v, 0, n - 1);
+          printf("%d ", v[i]);
+        }
 
-    int size = sizeof(v)/sizeof(v[0]);
-    radixSort(v, size);
-    exibeVet(v, n);
+        int size = sizeof(v)/sizeof(v[0]);
+        radixSort(v, size);
+        exibeVet(v, n);
+        heapSort(v, n);
+        exibeVet(v, n);
+    */
 
-
-    /*heapSort(v, n);
-    exibeVet(v, n);*/
-
+    Menu();
+    int v;
     return 0;
 }
 
-//Funções
+void Menu(){
+    int opcao = 9;
+    int num;
+    while( opcao != 0){
+        system("clear");
+        printf("1 - Radix Sort\n");
+        printf("2 - Quick Sort\n");
+        printf("3 - Heap Sort\n");
+        printf("4 - Merge Sort\n");
+        printf("5 - Buble Sort\n");
+        printf("6 - Selection Sort\n");
+        printf("7 - Isertion Sort\n");
+        printf("8 - Shell Sort\n");
+        printf("9 - Media Todas\n");
+        printf("0 - Sair\n");
+        scanf("%d", &opcao);
+        switch(opcao)
+        {
+            case 1:
+                num = submenu();
+                v = elementos(num);
+                bubleSort(v, num);
+                free(array);
+                break;
+            case 2:
+                oi();
+                break;
+            case 3:
+                oi();
+                break;
+            case 4:
+                oi();
+                break;
+            case 5:
+                oi();
+                break;
+            case 6:
+                oi();
+                break;
+            case 7:
+                oi();
+                break;
+            case 8:
+                oi();
+                break;
+            case 9:
+                oi();
+                break;
+        }
+    }
+    sair();
+}
+
+int submenu(){
+    int op;
+    system("clear");
+    printf("Escolha um valor: \n");
+    printf("1 - 1.000\n");
+    printf("2 - 5.000\n");
+    printf("3 - 10.00\n");
+    printf("4 - 50.00\n");
+    printf("5 - 100.000\n");
+    scanf("%d",&op);
+
+    switch(op){
+    case 1:
+        return 1000;
+        break;
+    case 2:
+        return 5000;
+        break;
+    case 3:
+        return 10000;
+        break;
+    case 4:
+        return 50000;
+        break;
+    case 5:
+        return 100000;
+        break;
+
+    }
+}
+
+void oi(){
+    int op;
+    printf("Oi!\n");
+    printf("1 - Continuar Programa\n");
+    printf("2 - Finalizar Programa\n");
+    scanf("%d", &op);
+    (op == 1) ? Menu() : sair();
+}
+
+void elementos(num){
+    int i;
+    int *array;
+    array = malloc (num* sizeof (int));
+    for (i=0; i < num; i++) {
+       array[i] = rand();
+       printf("%d\n", i);
+    }
+
+    //printf("feito");
+    getchar();
+    getchar();
+    return array;
+}
+
+void sair(){
+    system("clear");
+    printf("\n\n\n\n\n\n");
+    printf("=============Obrigado  por Utilizar Nosso Programa ! ==============!");
+    printf("\n\n\n\n\n\n");
+    exit(0);
+}
+
+
+///Algoritimos de ordenação
 void bubleSort(int *v,int n){
    int i, continua, aux,fim = n,count=0;
    do{
@@ -320,3 +437,5 @@ void exibeVet(int *vet, int n) {
 		printf("%d ", vet[i]);
 	}
 }
+
+
